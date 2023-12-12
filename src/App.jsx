5a8d00318b3ac422a4796c45adab040adb6b2fd7
JackW0rld <marionetah7@gmail.com>
2023-12-11 22:52:33 -0400

    Added api image upload functionality and basic frontend implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import './App.css'
 
-function App() {
-
+const App = () => {
+  const [imagen, setImagen] = useState(null)
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setImagen(file);
+  }
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('imagen', imagen)
+    try {
+      const response = await fetch('http://localhost:3001/upload', {
+        method: 'POST',
+        body: formData,
+      })
+      const data = await response.json();
+      console.log('Imagen subida:', data.imagen);
+    } catch (error) {
+      console.error('Error al subir la imagen:', error);
+    }
+  }
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
-}
+    <div className='card-style'>
+      <input className='buttons-style' type="file" accept=".web, .jpg, jpeg, png" onChange={handleFileChange} />
+      <br />
+      <button className='buttons-style' onClick={handleUpload}>Subir Imagen</button>
+    </div>
+  );
+};
 
 export default App
--- src/server.js
+++ src/server.js
@@ -1,8 +1,54 @@
-import express from 'express'
+import express from 'express';
+import { Sequelize, DataTypes } from 'sequelize';
+import multer from 'multer';
 
 const app = express();
-const port = 3000;
+const PORT = 3001;
 
+const sequelize = new Sequelize('files_db', 'DESKTOP-3HTMILH/JAKC', null, {
+    host: 'localhost',
+    dialect: 'mssql',
+});
 
+const Imagen = sequelize.define('Imagen', {
+     nombre: {
+       type: DataTypes.STRING,
+    },
+     ruta: {
+       type: DataTypes.STRING,
+    },
+   });
 
-app.listen(() => console.log(`server on port in http//:localhost:${port}`))
+   // Configuración de Multer para manejar archivos
+const storage = multer.diskStorage({
+     destination: (req, file, cb) => {
+       cb(null, 'uploads/');
+    },
+     filename: (req, file, cb) => {
+       cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+   // Ruta para manejar la carga de imágenes
+app.post('/upload', upload.single('imagen'), async (req, res) => {
+  try {
+    const nuevaImagen = await Imagen.create({
+      nombre: req.file.originalname,
+      ruta: req.file.path,
+    });
+    res.json({ imagen: nuevaImagen });
+    console.log('Imagen subida con exito');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al subir la imagen' });
+    console.log('Error al subir la imagen');
+  }
+});
+
+  // Inicializando servidor
+app.listen(PORT, async () => {
+  await sequelize.sync(); // Sincronizar modelos con la base de datos
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
